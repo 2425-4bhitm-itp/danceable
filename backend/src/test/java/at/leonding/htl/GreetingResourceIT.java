1931cd753b04ebe2eec47edd32bd975542c4a7dd
KnechtenSami <s.mayer@students.htl-leonding.ac.at>
2024-09-25 17:18:35 +0200

    ADD [backend]: adding quarkus backend

---
+++ backend/src/test/java/at/leonding/htl/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package at.leonding.htl;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
