4b6ba55e3a00443e07e3c9ede7a06321e38ee93a
Tobias-Huch <tobias.huch@gmx.at>
2024-11-25 22:18:59 +0100

    ADD [backend]: Added FileUploadResource.uploadFile API Route with a "File" Query Param

--- code/backend/src/main/java/at/leonding/htl/features/upload/FileUploadResource.java
+++ code/backend/src/main/java/at/leonding/htl/features/upload/FileUploadResource.java
@@ -5,16 +5,14 @@ import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 import jakarta.inject.Inject;
 import jakarta.transaction.Transactional;
-import jakarta.ws.rs.Consumes;
-import jakarta.ws.rs.POST;
-import jakarta.ws.rs.Path;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import org.jboss.resteasy.annotations.providers.multipart.MultipartForm;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.util.List;
 import java.util.Map;
 import javax.sound.sampled.UnsupportedAudioFileException;
@@ -52,6 +50,35 @@ public class FileUploadResource {
             return Response
                     .status(Response.Status.INTERNAL_SERVER_ERROR)
                     .entity("Error processing file").build();
+        }
+    }
+
+    @GET
+    @Path("/file")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getFourierDataOfFile(@QueryParam("filePath") String filePath){
+        try {
+            File file = new File(filePath);
+            InputStream stream = new FileInputStream(file);
+
+            fourierAnalysis.calculateValues(stream);
+
+            Map<Double, Double> frequencyMagnitudeMap = fourierAnalysis.getFrequencyMagnitudeMap();
+            double bpm = fourierAnalysis.getBpm();
+            List<String> danceTypes = fourierAnalysis.getDanceTypes();
+
+            JsonNodeFactory factory = JsonNodeFactory.instance;
+            ObjectNode json = factory.objectNode();
+
+            json.put("bpm", bpm);
+            json.put("danceTypes", danceTypes.toString());
+            json.put("frequencyMagnitudeMap", frequencyMagnitudeMap.toString());
+
+            return Response
+                    .ok(json)
+                    .build();
+        } catch (UnsupportedAudioFileException | IOException e) {
+            throw new RuntimeException(e);
         }
     }
 }
