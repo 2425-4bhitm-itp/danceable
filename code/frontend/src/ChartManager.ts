b064384e8de19f9f0184a73d09adebdd5694ba47
Samuel <s.mayer@students.htl-leonding.ac.at>
2024-12-02 08:28:32 +0100

    MOD[frontend]: improve ChartManager.ts

--- code/frontend/src/ChartManager.ts
+++ code/frontend/src/ChartManager.ts
@@ -1,11 +1,6 @@
-import { Chart, ChartData, ChartItem, ChartOptions } from "chart.js/auto";
-
-interface UploadFileResponse {
-  bpm: number;
-  danceTypes: string[];
-  frequencies: number[];
-  magnitudes: number[];
-}
+import {
+  Chart, ChartData, ChartItem, ChartOptions
+} from "chart.js/auto";
 
 export class ChartManager {
   charts: Chart[] = [];
@@ -19,14 +14,14 @@ export class ChartManager {
       x: {
         title: {
           display: true,
-          text: "Frequency (Hz)"
+          text: 'Frequency (Hz)'
         },
         beginAtZero: true
       },
       y: {
         title: {
           display: true,
-          text: "Magnitude"
+          text: 'Magnitude'
         }
       }
     }
@@ -41,10 +36,10 @@ export class ChartManager {
       {
         labels: frequencies,
         datasets: [{
-          label: "Magnitude vs Frequency",
+          label: 'Magnitude vs Frequency',
           data: magnitudes,
-          borderColor: "rgba(75, 192, 192, 1)",
-          backgroundColor: "rgba(75, 192, 192, 0.2)",
+          borderColor: 'rgba(75, 192, 192, 1)',
+          backgroundColor: 'rgba(75, 192, 192, 0.2)',
           tension: 0.1,
           fill: false
         }]
@@ -64,7 +59,7 @@ export class ChartManager {
     this.canvases = document.getElementsByClassName(this.canvasesClassName) as HTMLCollectionOf<HTMLCanvasElement>;
 
     for (let i = 0; i < Math.min(this.canvases.length, this.chartDatas.length); i++) {
-      const ctx = (this.canvases[i] as HTMLCanvasElement).getContext("2d");
+      const ctx = (this.canvases[i] as HTMLCanvasElement).getContext('2d');
 
       console.log(ctx);
       console.log(this.chartDatas[i]);
@@ -72,10 +67,10 @@ export class ChartManager {
       if (ctx) {
         this.charts.push(
           new Chart(ctx as ChartItem, {
-            type: "line",
-            data: this.chartDatas[i],
-            options: this.options
-          }));
+          type: 'line',
+          data: this.chartDatas[i],
+          options: this.options
+        }))
       }
     }
   }
@@ -83,14 +78,19 @@ export class ChartManager {
   async addDataSetFromAPI() {
     const data = await this.fetchDataFromAPI();
 
-    const frequencies: number[] = data.frequencies;
-    const magnitudes: number[] = data.magnitudes;
+    const frequencies: number[] = [];
+    const magnitudes: number[] = [];
+
+    for (const [key, value] of Object.entries(data)) {
+      frequencies.push(parseFloat(key)); // Convert the frequency (key) to a number
+      magnitudes.push(value as number); // Ensure the magnitude (value) is a number
+    }
 
     this.addDataSet(frequencies, magnitudes);
   }
 
-  async fetchDataFromAPI(): Promise<UploadFileResponse> {
-    const url = "/api/upload/file?filePath=/Users/samuelmayer/Downloads/long_footprints.wav";
+  private async fetchDataFromAPI(): Promise<any> {
+    const url = "/api/upload/file";
 
     try {
       const response = await fetch(url);
@@ -101,11 +101,9 @@ export class ChartManager {
 
       const data = await response.json();
 
-      console.log(data);
-
-      return data;
+      return data.frequencyMagnitudeMap;
     } catch (error) {
-      throw new Error("smth went wrong");
+      throw new Error("daschias mi");
     }
   }
 
@@ -119,7 +117,7 @@ export class ChartManager {
     this.charts = [];
 
     for (let i = 0; i < this.canvases.length; i++) {
-      const ctx = (this.canvases[i] as HTMLCanvasElement).getContext("2d");
+      const ctx = (this.canvases[i] as HTMLCanvasElement).getContext('2d');
 
       if (ctx && this.canvases[i]) {
         ctx.clearRect(0, 0, this.canvases[i].width, this.canvases[i].height);
@@ -128,16 +126,16 @@ export class ChartManager {
   }
 
   drawChartToCanvas(canvas: HTMLCanvasElement, dataSetIndex: number = 0) {
-    const ctx = canvas.getContext("2d");
+    const ctx = canvas.getContext('2d');
 
     if (ctx) {
       this.charts.push(
         new Chart(ctx as ChartItem, {
-          type: "line",
+          type: 'line',
           data: this.chartDatas[dataSetIndex],
           options: this.options
         })
-      );
+      )
     }
   }
 }
