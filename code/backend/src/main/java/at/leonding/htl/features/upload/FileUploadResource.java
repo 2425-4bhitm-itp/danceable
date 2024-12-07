f2e33cd1b365332dd52e0f9ea604e43876d3f685
Samuel <s.mayer@students.htl-leonding.ac.at>
2024-12-07 17:43:22 +0100

    ADD[backend]: added a concurrent version of /dir resource and extracted filling dto to separate methode

--- code/backend/src/main/java/at/leonding/htl/features/upload/FileUploadResource.java
+++ code/backend/src/main/java/at/leonding/htl/features/upload/FileUploadResource.java
@@ -2,18 +2,20 @@ package at.leonding.htl.features.upload;
 
 import at.leonding.htl.features.analyze.fourier.FourierAnalysis;
 import jakarta.inject.Inject;
-import jakarta.ws.rs.*;
-import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 
-import java.io.*;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Map;
-import java.util.TreeMap;
-import java.util.stream.Collectors;
 import javax.sound.sampled.UnsupportedAudioFileException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.*;
+import java.util.stream.Collectors;
 
 @Path("/upload")
 public class FileUploadResource {
@@ -33,32 +35,9 @@ public class FileUploadResource {
 
             fourierAnalysis.calculateValues(stream);
 
-            Map<Double, Double> frequencyMagnitudeMap = fourierAnalysis.getFrequencyMagnitudeMap();
-
-            Map<Double, Double> filteredMap = frequencyMagnitudeMap.entrySet().stream()
-                    .filter(entry -> entry.getKey() <= 5)
-                    .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
-
-            double bpm = fourierAnalysis.getBpm();
-            List<String> danceTypes = fourierAnalysis.getDanceTypes();
-
-            Map<Double, Double> sortedTreeMap = new TreeMap<>(filteredMap);
-
-
             return Response
                     .ok(
-                            new FourierAnalysisDataDto(
-                                    bpm,
-                                    danceTypes,
-                                    sortedTreeMap
-                                            .keySet()
-                                            .toArray(
-                                                    new Double[0]),
-                                    sortedTreeMap
-                                            .values()
-                                            .toArray(new Double[0]),
-                                    file.getName()
-                            )
+                            fillFourierAnalysisDataDto(file)
                     )
                     .build();
         } catch (UnsupportedAudioFileException | IOException e) {
@@ -110,6 +89,7 @@ public class FileUploadResource {
     public Response getFourierFromDirectory(
             @QueryParam("dirPath") String dirPath
     ) {
+        Long startTime = System.currentTimeMillis();
         File dir = new File(dirPath);
         File[] files = dir.listFiles();
 
@@ -120,49 +100,117 @@ public class FileUploadResource {
         List<FourierAnalysisDataDto> fourierAnalysisDataDtos = new LinkedList<>();
 
         for (int i = 0; i < files.length; i++) {
-            File file = files[i];
-            if (
-                    file.isFile() &&
-                            file.getName().endsWith(".wav")
-            ) {
-                try {
-                    InputStream stream = new FileInputStream(file);
+            FourierAnalysisDataDto fourierAnalysisDataDto = fillFourierAnalysisDataDto(files[i]);
 
-                    fourierAnalysis.calculateValues(stream);
-
-                    Map<Double, Double> frequencyMagnitudeMap = fourierAnalysis.getFrequencyMagnitudeMap();
-
-                    Map<Double, Double> filteredMap = frequencyMagnitudeMap.entrySet().stream()
-                            .filter(entry -> entry.getKey() <= 5)
-                            .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
-
-                    double bpm = fourierAnalysis.getBpm();
-                    List<String> danceTypes = fourierAnalysis.getDanceTypes();
-
-                    Map<Double, Double> sortedTreeMap = new TreeMap<>(filteredMap);
-
-                    fourierAnalysisDataDtos.add(
-                            new FourierAnalysisDataDto(
-                                bpm,
-                                danceTypes,
-                                sortedTreeMap
-                                        .keySet()
-                                        .toArray(
-                                                new Double[i]),
-                                sortedTreeMap
-                                        .values()
-                                        .toArray(new Double[i]),
-                                file.getName()
-                            )
-                    );
-                } catch (UnsupportedAudioFileException | IOException e) {
-                    throw new RuntimeException(e);
-                }
+            if (fourierAnalysisDataDto != null) {
+                System.out.println((i + 1) + ". fourier analysis is finished!");
+                fourierAnalysisDataDtos.add(fourierAnalysisDataDto);
             }
         }
+
+        Long endTime = System.currentTimeMillis();
+
+        System.out.println((startTime - endTime) / 1000 + " seconds elapsed for analyzing " + fourierAnalysisDataDtos.size() + " out of " + files.length + " files in directory.");
 
         return Response
                 .ok(fourierAnalysisDataDtos)
                 .build();
+    }
+
+    @GET
+    @Path("/dir-concurrent")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getFourierFromDirectoryConcurrent(
+            @QueryParam("dirPath") String dirPath
+    ) {
+        Long startTime = System.currentTimeMillis();
+        File dir = new File(dirPath);
+        File[] files = dir.listFiles();
+
+        if (files == null) {
+            throw new RuntimeException("No files found in directory");
+        }
+
+        List<FourierAnalysisDataDto> concurrentFourierAnalysisDataDtos = Collections.synchronizedList(new ArrayList<>());
+        List<Thread> threads = new ArrayList<>();
+
+        for (File file : files) {
+            Thread thread = new Thread(() -> {
+                FourierAnalysis fourierAnalysis = new FourierAnalysis();
+
+                FourierAnalysisDataDto fourierAnalysisDataDto = fillFourierAnalysisDataDto(file, fourierAnalysis);
+
+                if (fourierAnalysisDataDto != null) {
+                    System.out.println("A file has been fourier analyzed.");
+                    concurrentFourierAnalysisDataDtos.add(fourierAnalysisDataDto);
+                }
+            });
+
+            threads.add(thread);
+            thread.start();
+        }
+
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                throw new RuntimeException("Error while waiting for thread completion", e);
+            }
+        }
+
+        Long endTime = System.currentTimeMillis();
+
+        System.out.println((startTime - endTime) / 1000 + " seconds elapsed for analyzing " + concurrentFourierAnalysisDataDtos.size() + " out of " + files.length + " files in directory.");
+
+        return Response
+                .ok(concurrentFourierAnalysisDataDtos)
+                .build();
+    }
+
+    private FourierAnalysisDataDto fillFourierAnalysisDataDto(File file) {
+        return fillFourierAnalysisDataDto(file, this.fourierAnalysis);
+    }
+
+    private FourierAnalysisDataDto fillFourierAnalysisDataDto(File file, FourierAnalysis fourierAnalysis) {
+        FourierAnalysisDataDto fourierAnalysisDataDto = null;
+
+        if (
+                file.isFile() &&
+                        file.getName().endsWith(".wav")
+        ) {
+            try {
+                InputStream stream = new FileInputStream(file);
+
+                fourierAnalysis.calculateValues(stream);
+
+                Map<Double, Double> frequencyMagnitudeMap = fourierAnalysis.getFrequencyMagnitudeMap();
+
+                Map<Double, Double> filteredMap = frequencyMagnitudeMap.entrySet().stream()
+                        .filter(entry -> entry.getKey() <= 5)
+                        .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue));
+
+                double bpm = fourierAnalysis.getBpm();
+                List<String> danceTypes = fourierAnalysis.getDanceTypes();
+
+                Map<Double, Double> sortedTreeMap = new TreeMap<>(filteredMap);
+
+                fourierAnalysisDataDto = new FourierAnalysisDataDto(
+                        bpm,
+                        danceTypes,
+                        sortedTreeMap
+                                .keySet()
+                                .toArray(
+                                        new Double[0]),
+                        sortedTreeMap
+                                .values()
+                                .toArray(new Double[0]),
+                        file.getName()
+                );
+            } catch (UnsupportedAudioFileException | IOException e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        return fourierAnalysisDataDto;
     }
 }
