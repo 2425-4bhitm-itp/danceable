92dd834aff736fbf1e09afb3313828dfaf68f69f
Samuel <s.mayer@students.htl-leonding.ac.at>
2024-12-10 14:05:00 +0100

    MOD[documentation]: extracting to .getAudioFile() function

--- code/frontend/src/classes/StreamRecorder.ts
+++ code/frontend/src/classes/StreamRecorder.ts
@@ -1,96 +1,97 @@
 export class StreamRecorder {
-  mediaRecorder: MediaRecorder | null = null;
-  audioChunks: Blob[] = [];
-  audioBlob: Blob | null = null;
+    mediaRecorder: MediaRecorder | null = null;
+    audioChunks: Blob[] = [];
+    audioBlob: Blob | null = null;
 
-  async startRecording() {
-    try {
-      // Request permission and access to the microphone
-      const stream: MediaStream = await navigator.mediaDevices.getUserMedia({
-        audio: {
-          echoCancellation: true
+    async startRecording() {
+        try {
+            // Request permission and access to the microphone
+            const stream: MediaStream = await navigator.mediaDevices.getUserMedia({
+                audio: {
+                    echoCancellation: true
+                }
+            });
+
+            this.mediaRecorder = new MediaRecorder(stream);
+
+            this.audioChunks = [];
+
+            this.mediaRecorder.start();
+
+            this.mediaRecorder.addEventListener("dataavailable", (event: BlobEvent) => {
+                this.audioChunks.push(event.data);
+            });
+
+            this.mediaRecorder.addEventListener("stop", () => {
+                if (this.audioChunks.length > 0) {
+                    this.audioBlob = new Blob(this.audioChunks, {type: "audio/webm"});
+                }
+            });
+        } catch (error) {
+            console.error("Error accessing microphone:", error);
         }
-      });
-
-      this.mediaRecorder = new MediaRecorder(stream);
-
-      this.audioChunks = [];
-
-      this.mediaRecorder.start();
-
-      this.mediaRecorder.addEventListener("dataavailable", (event: BlobEvent) => {
-        this.audioChunks.push(event.data);
-      });
-
-      this.mediaRecorder.addEventListener("stop", () => {
-        if (this.audioChunks.length > 0) {
-          this.audioBlob = new Blob(this.audioChunks, { type: "audio/webm" });
-        }
-      });
-    } catch (error) {
-      console.error("Error accessing microphone:", error);
     }
-  }
 
-  async stopRecording(): Promise<void> {
-    return new Promise<void>((resolve) => {
-      if (this.mediaRecorder && this.mediaRecorder.state !== "inactive") {
-        const tracks = this.mediaRecorder.stream.getTracks();
-        tracks.forEach(track => track.stop());
+    async stopRecording(): Promise<void> {
+        return new Promise<void>((resolve) => {
+            if (this.mediaRecorder && this.mediaRecorder.state !== "inactive") {
+                const tracks = this.mediaRecorder.stream.getTracks();
+                tracks.forEach(track => track.stop());
 
-        this.mediaRecorder.addEventListener("stop", () => {
-          if (this.audioChunks.length > 0) {
-            this.audioBlob = new Blob(this.audioChunks, { type: "audio/webm" });
-            console.log("Audio Blob created successfully");
-          }
-          resolve();  // Resolve the promise when the stop event is fully handled
+                this.mediaRecorder.addEventListener("stop", () => {
+                    if (this.audioChunks.length > 0) {
+                        this.audioBlob = new Blob(this.audioChunks, {type: "audio/webm"});
+                        console.log("Audio Blob created successfully");
+                    }
+                    resolve();  // Resolve the promise when the stop event is fully handled
+                });
+
+                this.mediaRecorder.stop();
+            } else {
+                resolve();  // If mediaRecorder is already inactive, resolve immediately
+            }
         });
+    }
 
-        this.mediaRecorder.stop();
-      } else {
-        resolve();  // If mediaRecorder is already inactive, resolve immediately
-      }
-    });
-  }
+    async getAudioFile(): Promise<File> {
+        if (!this.audioBlob) {
+            throw new Error("No audio blob to be found.");
+        }
 
-  async sendRecordedAudio(): Promise<void> {
-    if (this.audioBlob) {
-      console.log("sending recording");
-      const url = "/api/upload";
-      const file = new File([this.audioBlob!], "recorded_audio.webm", { type: "audio/webm" });
-      console.log(file);
+        return new File([this.audioBlob!], "recorded_audio.webm", {type: "audio/webm"});
+    }
 
-      const formData = new FormData();
-      formData.append("file", file);
-      formData.append("fileName", "recorded_audio.webm");
+    async sendRecordedAudio(): Promise<void> {
+        const file = await this.getAudioFile();
 
-      try {
+        console.log("sending recording");
+        const url = "/api/upload";
+        console.log(file);
+
+        const formData = new FormData();
+        formData.append("file", file);
+        formData.append("fileName", "recorded_audio.webm");
+
         const response = await fetch(url, {
-          method: "POST",
-          body: formData
+            method: "POST",
+            body: formData
         });
 
         if (!response.ok) {
-          throw new Error("Failed to upload file");
+            throw new Error("Failed to upload file" + response.statusText);
         }
 
         const responseData = await response.json();
         console.log("Response:", responseData);
-      } catch (error) {
-        console.error("Error uploading file:", error);
-      }
-    } else {
-      console.error("No blob could be found");
     }
-  }
 
-  playAudio(): void {
-    if (this.audioBlob) {
-      const audio = new Audio(URL.createObjectURL(this.audioBlob));
-      audio.volume = 0.5;
-      audio.play();
-    } else {
-      console.error("No audio blob available to play");
+    playAudio(): void {
+        if (this.audioBlob) {
+            const audio = new Audio(URL.createObjectURL(this.audioBlob));
+            audio.volume = 0.5;
+            audio.play();
+        } else {
+            console.error("No audio blob available to play");
+        }
     }
-  }
 }
