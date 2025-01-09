8f98bf69c82464350f18614846b0ad0393a44e80
LZinhobel <l.zinhobel@students.htl-leonding.ac.at>
2025-01-09 11:29:17 +0100

    backend[MOD] changed import statements

--- code/backend/src/main/java/at/leonding/htl/features/upload/ReadFile.java
+++ code/backend/src/main/java/at/leonding/htl/features/upload/ReadFile.java
@@ -1,36 +1,10 @@
 package at.leonding.htl.features.upload;
 
-import java.io.BufferedInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-
-import javax.sound.sampled.AudioFormat;
-import javax.sound.sampled.AudioInputStream;
-import javax.sound.sampled.AudioSystem;
-import javax.sound.sampled.UnsupportedAudioFileException;
+import javax.sound.sampled.*;
+import java.io.*;
 
 public class ReadFile {
-    public static InputStream convertWebmToWav(InputStream inputStream) throws IOException {
-        ProcessBuilder processBuilder = new ProcessBuilder("ffmpeg", "-i", "pipe:0", "-f", "wav", "pipe:1");
-        processBuilder.redirectErrorStream(true);
-        Process process = processBuilder.start();
 
-        Thread inputThread = new Thread(() -> {
-            try {
-                byte[] buffer = new byte[1024];
-                int bytesRead;
-                while ((bytesRead = inputStream.read(buffer)) != -1) {
-                    process.getOutputStream().write(buffer, 0, bytesRead);
-                }
-                process.getOutputStream().close();
-            } catch (IOException e) {
-                throw new RuntimeException(e);
-            }
-        });
-        inputThread.start();
-
-        return process.getInputStream();
-    }
 
     public static double[] readWavFile(InputStream inputStream) throws UnsupportedAudioFileException, IOException {
         BufferedInputStream bufferedInputStream = new BufferedInputStream(inputStream);
