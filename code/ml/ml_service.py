7d54671373d74240eb510da54227c4a3ba850453
LZinhobel <l.zinhobel@students.htl-leonding.ac.at>
2025-01-31 13:36:46 +0100

    MOD[docker]: changed python port to 5001

--- code/ml/ml_service.py
+++ code/ml/ml_service.py
@@ -82,4 +82,4 @@ def health_check():
     return jsonify(status="healthy", message="Service is running"), 200
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True, threaded=True)
+    app.run(host='0.0.0.0', port=5001, debug=True, threaded=True)
