b6afbb6cd05ad7f046c57657fd665df6f75c11cb
Samuel <s.mayer@students.htl-leonding.ac.at>
2025-03-17 09:55:41 +0100

    MOD[control-panel]: style menu

--- code/control-panel/src/components/menu/menu.ts
+++ code/control-panel/src/components/menu/menu.ts
@@ -19,15 +19,15 @@ class MenuElement extends HTMLElement {
     render(html`
       <div class="flex-initial p-2 h-full">
         <div class="flex justify-center items-center m-4">
-          <div class="w-10"><img class="w-full" src="/assets/danceable-logo.svg" alt=""></div>
+          <div class="w-10"><img class="w-full" src="/public/danceable-logo.svg" alt=""></div>
         </div>
         <a class="rounded-2xl bg-gray-100 aspect-square p-4 m-2 flex justify-center items-center"
            href="${libraryRoute}">
-          <div class="w-6"><img class="w-full" src="/assets/library-icon.svg" alt=""></div>
+          <div class="w-6"><img class="w-full" src="/public/icons/library-icon.svg" alt=""></div>
         </a>
         <a class="rounded-2xl bg-gray-100 aspect-square p-2 m-2 flex justify-center items-center"
            href="${analysisRoute}">
-          <div class="w-5"><img class="w-full" src="/assets/analysis-icon.svg" alt=""></div>
+          <div class="w-6"><img class="w-full" src="/public/icons/analysis-icon.svg" alt=""></div>
         </a>
       </div>
     `, this)
