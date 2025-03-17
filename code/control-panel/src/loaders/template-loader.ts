ca15c1feccbbf1851a9dda0b22d78e5aadcd47cc
Samuel <s.mayer@students.htl-leonding.ac.at>
2025-03-17 01:37:55 +0100

    FIX[control-panel]: new prettier formatter

--- code/control-panel/src/loaders/template-loader.ts
+++ code/control-panel/src/loaders/template-loader.ts
@@ -1,18 +1,19 @@
 /** a webpack loader to load html-templates.
-  * (c) Christian Aberger (2025)
+ * (c) Christian Aberger (2025)
  * @author Christian Aberger
  * https://www.aberger.at
  */
 
 function template<T>(content: T) {
-    const code = `
+  const code = `
         function template(model) {
             const templateElement = document.createElement("template")
             templateElement.innerHTML = \`${content}\`
             return templateElement
         }
     `
-    const exportString = "module.exports = " + code
-    return exportString
+  const exportString = 'module.exports = ' + code
+  return exportString
 }
+
 export default template
