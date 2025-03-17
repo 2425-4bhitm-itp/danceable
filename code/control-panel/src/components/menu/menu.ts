eb5616335451a7c8a4d9fc2d0b0a494c6468ab50
Samuel <s.mayer@students.htl-leonding.ac.at>
2025-03-17 23:33:18 +0100

    ADD[control-panel]: highlight link

--- code/control-panel/src/components/menu/menu.ts
+++ code/control-panel/src/components/menu/menu.ts
@@ -3,6 +3,7 @@ import { addLinks } from 'lib/router'
 
 import { libraryRoute } from 'components/library'
 import { analysisRoute } from 'components/analysis'
+import { subscribe } from 'model/model'
 
 export const AppMenu = 'menu-component'
 
@@ -13,25 +14,49 @@ class MenuElement extends HTMLElement {
 
   connectedCallback() {
     this.render()
+
+    subscribe((model) => this.highlightLink(model.currentPane))
   }
 
   render() {
     render(html`
       <div class="flex-initial p-2 h-full">
         <div class="flex justify-center items-center m-4">
-          <div class="w-10"><img class="w-full" src="/public/danceable-logo.svg" alt=""></div>
+          <div class="h-12"><img class="h-full" src="/public/danceable-logo.svg" alt=""></div>
         </div>
-        <a class="rounded-2xl bg-gray-100 aspect-square p-4 m-2 flex justify-center items-center hover:bg-gray-200 transition"
-           href="${libraryRoute}">
-          <div class="w-6"><img class="w-full" src="/public/icons/library-icon.svg" alt=""></div>
+        <a
+          class="rounded-2xl bg-gray-100 aspect-square p-4 m-2 flex justify-center items-center hover:bg-gray-200 transition"
+          href="${libraryRoute}">
+          <div class="w-6"><img class="w-full" src="/public/icons/library-icon-dark.svg" alt=""></div>
         </a>
-        <a class="rounded-2xl bg-gray-100 aspect-square p-2 m-2 flex justify-center items-center hover:bg-gray-200 transition"
-           href="${analysisRoute}">
-          <div class="w-6"><img class="w-full" src="/public/icons/analysis-icon.svg" alt=""></div>
+        <a
+          class="rounded-2xl bg-gray-100 aspect-square p-2 m-2 flex justify-center items-center hover:bg-gray-200 transition"
+          href="${analysisRoute}">
+          <div class="w-6"><img class="w-full" src="/public/icons/analysis-icon-dark.svg" alt=""></div>
         </a>
       </div>
     `, this)
+
     addLinks(this)
+  }
+
+
+  private highlightLink(currentPane: string) {
+    const links = this.querySelectorAll('a')
+
+    links.forEach(link => {
+      link.classList.remove('bg-gray-dark')
+      link.classList.remove('hover:bg-gray-200')
+
+      link.querySelector('img').src = link.querySelector('img').src.replace('light', 'dark');
+
+      if ('/' + link.getAttribute('href') === currentPane) {
+        link.classList.add('bg-gray-dark')
+        link.querySelector('img').src = link.querySelector('img').src.replace('dark', 'light');
+      } else {
+        link.classList.add('hover:bg-gray-200')
+      }
+    })
   }
 }
 
