ac0df9349c3705edb26bf2d0348bb71e3f0d5e2e
thiszoe <z.oellinger@students.htl-leonding.ac.at>
2025-03-24 13:06:14 +0100

    add: create component record-svg

---
+++ code/angular-frontend/src/app/record-svg/record-svg.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-record-svg',
+  imports: [],
+  templateUrl: './record-svg.component.html',
+  styleUrl: './record-svg.component.css'
+})
+export class RecordSvgComponent {
+
+}
