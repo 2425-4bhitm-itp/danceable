267b7ab29c5e0eefcdb9baf36e3b98451923f48a
thiszoe <z.oellinger@students.htl-leonding.ac.at>
2025-03-24 13:06:27 +0100

    add: create component visualizer

---
+++ code/angular-frontend/src/app/visualizer/visualizer.component.ts
@@ -0,0 +1,74 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-visualizer',
+  imports: [],
+  templateUrl: './visualizer.component.html',
+  styleUrl: './visualizer.component.css'
+})
+export class VisualizerComponent {
+  // private audioContext: AudioContext;
+  // private analyser: AnalyserNode;
+  // private processFrame: (data: Uint8Array) => void;
+  // private animationFrameId: number = 0;
+  //
+  // //audioContext object for processing audio
+  // //processFrame = function for processing audio file
+  // // processerror = a function for handeling errors
+  //
+  // constructor(audioContext: AudioContext, processFrame: (data: Uint8Array) => void, processError?: (error: Error) => void) {
+  //   this.audioContext = audioContext;
+  //   this.analyser = this.audioContext.createAnalyser();
+  //   this.processFrame = processFrame;
+  //   this.connectStream = this.connectStream.bind(this);
+  //   navigator.mediaDevices.getUserMedia({ audio: true, video: false })
+  //     // getUserMedia API is used to get access to the users microphone
+  //     .then(this.connectStream)
+  //     //if allowed use connectStream
+  //     .catch((error) => {
+  //       if (processError) {
+  //         processError(error);
+  //       }
+  //     });
+  // }
+
+  // connectStream(stream: MediaStream) {
+  //   // if stream is available call connectStream method
+  //   // Make an AnalyserNode to analyse the audio
+  //   const source = this.audioContext.createMediaStreamSource(stream);
+  //   //MediaStreamSource is being made out of the audio stream
+  //   source.connect(this.analyser);
+  //   // Connecting audio-stream and analyser
+  //   this.analyser.smoothingTimeConstant = 0.5;
+  //   this.analyser.fftSize = 32;
+  //   //smoothing and fft size = anzahl frequenzbaender
+  //
+  //   this.initRenderLoop();
+  // }
+  //
+  //
+  // private initRenderLoop() {
+  //   const frequencyData = new Uint8Array(this.analyser.frequencyBinCount);
+  //   //create array storing frequency data
+  //   const processFrame = this.processFrame || (() => {
+  //   });
+  //   //if no process frame metod, use empty one
+  //
+  //   const renderFrame = () => {
+  //     this.analyser.getByteFrequencyData(frequencyData);
+  //     //frequency data update in array
+  //     processFrame(frequencyData);
+  //     //processframe function is called with updated data
+  //
+  //     this.animationFrameId = requestAnimationFrame(renderFrame);
+  //     //get next animation loop
+  //   };
+  //   this.animationFrameId = requestAnimationFrame(renderFrame);
+  //   //starting animation
+  // }
+  //
+  // stopVisualizer() {
+  //   cancelAnimationFrame(this.animationFrameId);
+  // }
+}
+
