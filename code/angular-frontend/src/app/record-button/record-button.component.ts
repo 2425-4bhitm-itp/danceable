dc70560070d63e2390979410dde74cada3a024d8
thiszoe <z.oellinger@students.htl-leonding.ac.at>
2025-03-24 13:08:58 +0100

    fix: add buttonState, svg and visualizer component to fix logic issues

--- code/angular-frontend/src/app/record-button/record-button.component.ts
+++ code/angular-frontend/src/app/record-button/record-button.component.ts
@@ -1,11 +1,18 @@
 import { Component } from '@angular/core';
+import {RecordStopButtonComponent} from '../record-stop-button/record-stop-button.component';
+import {RecordSvgComponent} from '../record-svg/record-svg.component';
+import {VisualizerComponent} from '../visualizer/visualizer.component';
 
 @Component({
   selector: 'app-record-button',
-  imports: [],
+  imports: [
+    RecordStopButtonComponent,
+    RecordSvgComponent,
+    VisualizerComponent
+  ],
   templateUrl: './record-button.component.html',
-  styleUrl: './record-button.component.css'
+  styleUrl: './record-button.component.css',
 })
 export class RecordButtonComponent {
-
+  buttonState = true
 }
