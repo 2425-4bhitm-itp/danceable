9eb52becba748617a32877404079dc4d847a2c85
Samuel <s.mayer@students.htl-leonding.ac.at>
2025-04-27 16:44:24 +0200

    MOD[ios]: move data fetching to library

--- code/frontend/src/App.tsx
+++ code/frontend/src/App.tsx
@@ -4,15 +4,7 @@ import Home from './pages/Home.tsx'
 
 import { createBrowserRouter, RouterProvider } from 'react-router'
 import RootLayout from './components/RootLayout.tsx'
-import { useDanceStore } from './stores/dance/danceStore.ts'
-import { useEffect } from 'react'
-import { useClipStore } from './stores/clip/clipStore.ts'
-import { useSongStore } from './stores/song/songStore.ts'
-import { ToastType } from './components/toast/ToastType.ts'
 import ToastContainer from './components/toast/ToastContainer.tsx'
-import { ToastData } from './stores/toast/ToastData.ts'
-import { useToastStore } from './stores/toast/toastStore.ts'
-import Toast from './components/toast/Toast.tsx'
 
 const router = createBrowserRouter([
   {
@@ -27,41 +19,6 @@ const router = createBrowserRouter([
 ])
 
 function App() {
-  const { fetchDances } = useDanceStore()
-  const { fetchClips } = useClipStore()
-  const { fetchSongs } = useSongStore()
-  const { createToast } = useToastStore()
-
-  const fetchData = async () => {
-    await fetchClips((message) =>
-      createToast({
-        type: ToastType.ERROR,
-        message: message,
-        timeToLive: 3000,
-      } as ToastData)
-    )
-
-    await fetchSongs((message) =>
-      createToast({
-        type: ToastType.ERROR,
-        message: message,
-        timeToLive: 3000,
-      } as ToastData)
-    )
-
-    await fetchDances((message) =>
-      createToast({
-        type: ToastType.ERROR,
-        message: message,
-        timeToLive: 3000,
-      } as ToastData)
-    )
-  }
-
-  useEffect(() => {
-    fetchData()
-  }, [])
-
   return (
     <>
       <ToastContainer />
--- code/frontend/src/pages/Library.tsx
+++ code/frontend/src/pages/Library.tsx
@@ -6,22 +6,69 @@ import { useSongStore } from '../stores/song/songStore'
 import AddSongModal from '../components/modals/AddSongModel'
 import EditSongModal from '../components/modals/EditSongModal'
 import SwitchSongModal from '../components/modals/SwitchSongModal'
+import AddClipModal from '../components/modals/AddClipModal'
+import { useDanceStore } from '../stores/dance/danceStore'
+import { useToastStore } from '../stores/toast/toastStore'
+import { ToastType } from '../components/toast/ToastType'
+import { ToastData } from '../stores/toast/ToastData'
+import { useEffect } from 'react'
+
+import uploadIcon from '../assets/icons/upload.svg'
 
 function Library() {
-  const clips = useClipStore((state) => state.clips)
+  const { clips, fetchClips, setIsAddingClip } = useClipStore()
   const danceFilterIds = useClipsFilter((state) => state.danceIds)
-  const { songs, setIsAddingSong } = useSongStore()
+  const { songs, setIsAddingSong, fetchSongs } = useSongStore()
+  const { fetchDances } = useDanceStore()
+  const { createToast } = useToastStore()
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await fetchClips((message) =>
+        createToast({
+          type: ToastType.ERROR,
+          message: message,
+          timeToLive: 3000,
+        } as ToastData)
+      )
+
+      await fetchSongs((message) =>
+        createToast({
+          type: ToastType.ERROR,
+          message: message,
+          timeToLive: 3000,
+        } as ToastData)
+      )
+
+      await fetchDances((message) =>
+        createToast({
+          type: ToastType.ERROR,
+          message: message,
+          timeToLive: 3000,
+        } as ToastData)
+      )
+    }
+
+    fetchData()
+  }, [])
 
   return (
     <>
       <EditSongModal />
       <SwitchSongModal />
+      <AddClipModal />
       <AddSongModal />
       <h1 className="px-2 pt-8 text-3xl">Library</h1>
-      <div className="flex flex-row-reverse px-4">
+      <div className="flex flex-row-reverse gap-1 px-4">
         <button
-          id="uploadSnippetButton"
-          className="bg-gray-dark cursor-pointer rounded-xl px-5 py-1 font-medium text-white"
+          className="bg-gray-dark flex cursor-pointer gap-2 rounded-xl px-5 py-1 font-medium text-white"
+          onClick={() => setIsAddingClip(true)}
+        >
+          <img className="w-5" src={uploadIcon} alt="upload icon" />
+          upload clips
+        </button>
+        <button
+          className="border-gray-dark cursor-pointer rounded-xl border-2 bg-white px-4 py-0.5 font-medium"
           onClick={() => setIsAddingSong(true)}
         >
           add song
