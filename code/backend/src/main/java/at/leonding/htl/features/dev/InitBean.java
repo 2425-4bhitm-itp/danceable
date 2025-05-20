a94d3ba364ea1fc8117f8ddc08376b92d0a6c320
Samuel <s.mayer@students.htl-leonding.ac.at>
2025-05-20 08:30:58 +0200

    MOD[backend]: matching dance names to ml

--- code/backend/src/main/java/at/leonding/htl/features/dev/InitBean.java
+++ code/backend/src/main/java/at/leonding/htl/features/dev/InitBean.java
@@ -2,6 +2,7 @@ package at.leonding.htl.features.dev;
 
 import at.leonding.htl.features.library.dance.Dance;
 import at.leonding.htl.features.library.dance.DanceRepository;
+import io.quarkus.logging.Log;
 import io.quarkus.runtime.LaunchMode;
 import io.quarkus.runtime.StartupEvent;
 import jakarta.enterprise.context.ApplicationScoped;
@@ -17,22 +18,19 @@ public class InitBean {
     @Transactional
     void startup(@Observes StartupEvent startupEvent) {
         if (LaunchMode.current() == LaunchMode.NORMAL) {
-            System.out.println("InitBean.startup");
+            Log.info("Do not use InitBean in prod!!!");
 
             danceRepository.persist(
-                    new Dance("Slow Waltz", 84, 90),
-                    new Dance("Viennese Waltz", 174, 180),
+                    new Dance("Discofox", 112, 120),
+                    new Dance("Slowwaltz", 84, 90),
+                    new Dance("Viennawaltz", 174, 180),
+                    new Dance("Chacha", 120, 128),
                     new Dance("Foxtrott", 112, 120),
                     new Dance("Quickstep", 200, 208),
-                    new Dance("Tango", 120, 140),
-                    new Dance("Cha Cha Cha", 120, 128),
+                    new Dance("Salsa", 180, 300),
                     new Dance("Rumba", 100, 108),
                     new Dance("Samba", 96, 104),
-                    new Dance("Jive", 168, 184),
-                    new Dance("Blues Dance", 20, 75),
-                    new Dance("Tango Argentino", 80, 160),
-                    new Dance("Salsa", 180, 300),
-                    new Dance("Bachata", 90, 200)
+                    new Dance("Jive", 168, 184)
             );
         }
     }
