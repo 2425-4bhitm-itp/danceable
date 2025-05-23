011e903e79ba916f11c05ff295270d1d17d61d60
silin-hussein <s.hussein@students.htl-leonding.ac.at>
2025-05-23 21:09:23 +0200

    don't know

--- code/backend/build.sh
+++ code/backend/build.sh
@@ -3,6 +3,5 @@
 # shellcheck disable=SC2164
 cd "$(dirname "$0")"
 
-mvn clean package -Dquarkus.config.locations=./src/main/resources/application.properties
+mvn clean install -Dquarkus.config.locations=./src/main/resources/application.properties
 
-docker build -t "backend" -f docker/Dockerfile .
