f7873dc60190dc17f0d10e1f6c48b24e882da605
thiszoe <z.oellinger@students.htl-leonding.ac.at>
2025-05-24 19:46:22 +0200

    FEAT[frontend]: creating a method, that starts and stops the animation and sets the current buttonState

--- code/angular-frontend/src/app/record-button/record-button.component.ts
+++ code/angular-frontend/src/app/record-button/record-button.component.ts
@@ -3,6 +3,8 @@ import {RecordStopButtonComponent} from '../record-stop-button/record-stop-butto
 import {RecordSvgComponent} from '../record-svg/record-svg.component';
 import {VisualizerComponent} from '../visualizer/visualizer.component';
 
+
+
 @Component({
   selector: 'app-record-button',
   imports: [
@@ -14,10 +16,29 @@ import {VisualizerComponent} from '../visualizer/visualizer.component';
   styleUrl: './record-button.component.css',
 })
 export class RecordButtonComponent {
-  isRecording: boolean = false;
+  visualizer: VisualizerComponent = new VisualizerComponent();
+  buttonState: boolean = false;
 
   toggleRecording() {
-    this.isRecording = !this.isRecording;
-    console.log(this.isRecording ? "Recording started" : "Recording stopped");
+    this.buttonState = !this.buttonState;
+    console.log(this.buttonState ? "Recording started" : "Recording stopped");
+    if(this.buttonState){
+      setTimeout(() => {
+        this.buttonState = false;
+        this.stopAnimation();
+        console.log("Recording stopped automatically after 5 seconds");
+      }, 5000);
+      this.stopAnimation()
+    } else {
+      this.startAnimation()
+    }
+  }
+
+  startAnimation(): void{
+    this.visualizer.startAnimation();
+  }
+
+  stopAnimation(): void{
+    this.visualizer.stopAnimation();
   }
 }
