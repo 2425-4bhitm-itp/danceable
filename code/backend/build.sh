822291a6c66868ad1b254d9cde993fd5133329e7
silin-hussein <s.hussein@students.htl-leonding.ac.at>
2025-09-16 11:45:37 +0200

    small changes to docker push action

--- code/backend/build.sh
+++ code/backend/build.sh
@@ -3,5 +3,10 @@
 # shellcheck disable=SC2164
 cd "$(dirname "$0")"
 
-mvn clean install -DskipTests -Dquarkus.config.locations=./src/main/resources/application.properties
+mvn clean package -DskipTests
 
+docker build -t backend:latest -f docker/Dockerfile .
+
+docker tag backend:latest ghcr.io/2425-4bhitm-itp/backend:latest
+
+docker push ghcr.io/2425-4bhitm-itp/backend:latest
