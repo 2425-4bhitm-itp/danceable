335d92aac8a9faf8c07b78de35e86b7725c3b8ad
LZinhobel <l.zinhobel@students.htl-leonding.ac.at>
2025-11-26 10:52:47 +0100

    FIX[ml]: changed ml files location

--- code/ml/config/paths.py
+++ code/ml/config/paths.py
@@ -2,17 +2,15 @@ from pathlib import Path
 
 # Base directory for all storage
 BASE_DIR = Path("/app/song-storage")
+BASE_MODEL_DIR = Path("/opt/model")
 
 # Model files
-MODEL_PATH = BASE_DIR / "model.keras"
-SCALER_PATH = BASE_DIR / "scaler.pkl"
-LABELS_PATH = BASE_DIR / "label_order.json"
-FEATURES_CSV = BASE_DIR / "features.csv"
-COREML_PATH = BASE_DIR / "model.mlmodel"
+MODEL_PATH = BASE_MODEL_DIR / "model.keras"
+SCALER_PATH = BASE_MODEL_DIR / "scaler.pkl"
+LABELS_PATH = BASE_MODEL_DIR / "label_order.json"
+COREML_PATH = BASE_MODEL_DIR / "model.mlmodel"
 
 # Dataset paths
+FEATURES_CSV = BASE_DIR / "features.csv"
 SONGS_DIR = BASE_DIR / "songs"
 SNIPPETS_DIR = SONGS_DIR / "snippets"
-
-# Default dataset output
-DEFAULT_FEATURES_CSV = FEATURES_CSV
