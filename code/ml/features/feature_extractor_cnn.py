af12bbdad14f83d241edd9b0b3288b080e61213a
LZinhobel <l.zinhobel@students.htl-leonding.ac.at>
2025-12-04 12:44:05 +0100

    FIX[ml]: fix dimension height problem

--- code/ml/features/feature_extractor_cnn.py
+++ code/ml/features/feature_extractor_cnn.py
@@ -9,126 +9,90 @@ class AudioFeatureExtractorCNN:
         n_fft=1024,
         hop_length=256,
         n_mels=128,
-        target_time_bins=128
+        n_mfcc=40,
+        target_time_bins=128,
+        target_height=128
     ):
         self.sr = sr
         self.window_seconds = window_seconds
-        self.window_len = window_seconds * sr
+        self.window_len = int(window_seconds * sr)
         self.n_fft = n_fft
         self.hop_length = hop_length
         self.n_mels = n_mels
+        self.n_mfcc = n_mfcc
         self.target_time_bins = target_time_bins
+        self.target_height = target_height
 
     def load_audio(self, file_path):
         y, _ = librosa.load(file_path, sr=self.sr)
-        y = librosa.util.normalize(y)
-        return y
+        return librosa.util.normalize(y)
 
     def slice_into_windows(self, y):
-        windows = []
-        step = self.window_len
-        total = len(y)
-        for start in range(0, total - self.window_len + 1, step):
-            end = start + self.window_len
-            windows.append(y[start:end])
-        return windows
+        return [y[i:i+self.window_len] for i in range(0, len(y)-self.window_len+1, self.window_len)]
 
     def _normalize(self, x):
         return (x - x.mean()) / (x.std() + 1e-8)
 
-    def _fix_time_axis(self, x):
-        return librosa.util.fix_length(x, size=self.target_time_bins, axis=1)
+    def _fix_shape(self, x):
+        # Fix height and time axes
+        x = librosa.util.fix_length(x, size=self.target_height, axis=0)
+        x = librosa.util.fix_length(x, size=self.target_time_bins, axis=1)
+        return x
 
-    def _mel(self, y):
-        mel = librosa.feature.melspectrogram(
-            y=y,
-            sr=self.sr,
-            n_fft=self.n_fft,
-            hop_length=self.hop_length,
-            n_mels=self.n_mels
-        )
+    def _extract_mel(self, y):
+        mel = librosa.feature.melspectrogram(y=y, sr=self.sr, n_fft=self.n_fft,
+                                             hop_length=self.hop_length, n_mels=self.n_mels)
         mel = librosa.power_to_db(mel)
         mel = self._normalize(mel)
-        mel = self._fix_time_axis(mel)
-        return mel.astype(np.float32)
+        return self._fix_shape(mel).astype(np.float32)
 
-    def _mfcc(self, y, n_mfcc=40):
-        mf = librosa.feature.mfcc(
-            y=y,
-            sr=self.sr,
-            n_fft=self.n_fft,
-            hop_length=self.hop_length,
-            n_mfcc=n_mfcc
-        )
-        mf = self._normalize(mf)
-        mf = self._fix_time_axis(mf)
-        return mf.astype(np.float32)
+    def _extract_mfcc(self, y):
+        mfcc = librosa.feature.mfcc(y=y, sr=self.sr, n_mfcc=self.n_mfcc,
+                                    n_fft=self.n_fft, hop_length=self.hop_length)
+        mfcc = self._normalize(mfcc)
+        return self._fix_shape(mfcc).astype(np.float32)
 
-    def _chroma(self, y):
-        chroma = librosa.feature.chroma_cqt(
-            y=y,
-            sr=self.sr
-        )
+    def _extract_chroma(self, y):
+        chroma = librosa.feature.chroma_cqt(y=y, sr=self.sr)
         chroma = self._normalize(chroma)
-        chroma = self._fix_time_axis(chroma)
-        return chroma.astype(np.float32)
+        return self._fix_shape(chroma).astype(np.float32)
 
-    def _spectral_contrast(self, y):
-        sc = librosa.feature.spectral_contrast(
-            y=y,
-            sr=self.sr,
-            n_fft=self.n_fft,
-            hop_length=self.hop_length
-        )
+    def _extract_spectral_contrast(self, y):
+        sc = librosa.feature.spectral_contrast(y=y, sr=self.sr, n_fft=self.n_fft, hop_length=self.hop_length)
         sc = self._normalize(sc)
-        sc = self._fix_time_axis(sc)
-        return sc.astype(np.float32)
+        return self._fix_shape(sc).astype(np.float32)
 
-    def _tempogram(self, y):
-        tgram = librosa.feature.tempogram(
-            y=y,
-            sr=self.sr,
-            hop_length=self.hop_length
-        )
+    def _extract_tempogram(self, y):
+        tgram = librosa.feature.tempogram(y=y, sr=self.sr, hop_length=self.hop_length)
         tgram = self._normalize(tgram)
-        tgram = self._fix_time_axis(tgram)
-        return tgram.astype(np.float32)
+        return self._fix_shape(tgram).astype(np.float32)
 
-    def _onset(self, y):
-        on = librosa.onset.onset_strength(
-            y=y,
-            sr=self.sr,
-            hop_length=self.hop_length
-        )
+    def _extract_onset(self, y):
+        on = librosa.onset.onset_strength(y=y, sr=self.sr, hop_length=self.hop_length)
         on = on[np.newaxis, :]
         on = self._normalize(on)
-        on = self._fix_time_axis(on)
-        return on.astype(np.float32)
+        return self._fix_shape(on).astype(np.float32)
 
     def _combine_features(self, feats):
-        stacked = np.concatenate(
-            [f[np.newaxis, ...] for f in feats],
-            axis=0
-        )
-        return stacked
+        # stack along channel axis: H x W x C
+        return np.stack(feats, axis=-1)
 
     def extract_features_from_file(self, path):
         y = self.load_audio(path)
         windows = self.slice_into_windows(y)
-
         outputs = []
-        for w in windows:
-            mel = self._mel(w)
-            mfcc = self._mfcc(w)
-            chroma = self._chroma(w)
-            spec_contrast = self._spectral_contrast(w)
-            tempogram = self._tempogram(w)
-            onset = self._onset(w)
 
-            combined = self._combine_features(
-                [mel, mfcc, chroma, spec_contrast, tempogram, onset]
-            )
+        for w in windows:
+            mel = self._extract_mel(w)
+            mfcc = self._extract_mfcc(w)
+            chroma = self._extract_chroma(w)
+            sc = self._extract_spectral_contrast(w)
+            tgram = self._extract_tempogram(w)
+            onset = self._extract_onset(w)
+
+            combined = self._combine_features([mel, mfcc, chroma, sc, tgram, onset])
             outputs.append(combined)
+
         return outputs
 
     def wav_to_spectrogram_tensor(self, file_path):
