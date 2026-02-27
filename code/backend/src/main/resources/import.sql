3eeed265589c5a68ab346ac20467750adea1f2b0
marvin-anderson <m.anderson@students.htl-leonding.ac.at>
2026-02-27 09:35:54 +0100

    FIX[backend]: import.sql fix again

--- code/backend/src/main/resources/import.sql
+++ code/backend/src/main/resources/import.sql
@@ -1,16 +1,16 @@
 -- Insert dances
  INSERT INTO Dance (name, minBpm, maxBpm, description)
  VALUES
-     ('Slow Waltz', 84, 90, "A graceful ballroom dance in 3/4 time with smooth, flowing rise-and-fall movements and elegant progress around the floor."),
-     ('Viennese Waltz', 174, 180, "A fast, rotating form of waltz in 3/4 time with continuous turns and sweeping movement around the floor; one of the oldest ballroom dances."),
-     ('Quickstep', 200, 208, "A brisk, flowing ballroom dance evolved from foxtrot, featuring running steps, syncopations, and light hops; danced in a smooth style with continuous forward movement."),
-     ('Tango', 200, 208, "A dramatic ballroom dance with sharp, staccato movements and purposeful pauses, expressing strong character and musical accents."),
-     ('Cha Cha Cha', 120, 128, "A lively Latin partner dance with syncopated “cha-cha-cha” steps, playful hip motion, and rhythm that breaks on the 4th and 1st beats."),
-     ('Rumba', 100, 108, "A slow, sensual Latin ballroom dance emphasizing hip action and romantic connection, with smooth, deliberate steps and steady rhythm."),
-     ('Samba', 96, 104, "A rhythmic Latin dance from Brazil with a distinctive bounce action and forward-backward steps, often seen in both social and competitive dance scenes."),
-     ('Jive', 168, 184, "A fast, energetic swing-influenced dance with quick rock-step and triple-step (shuffle) footwork, often danced to upbeat music in social and competitive settings."),
-     ('Salsa', 180, 300, "A popular Latin partner dance with energetic footwork, rhythmically syncopated timing, spins, and dynamic lead-follow patterns; danced in clubs worldwide."),
-     ('Discofox', 112, 120, "A versatile social partner dance from the disco era, combining simple forward-and-back or side steps with turns and wraps; danced to 4/4 music and easy to pick up by beginners."),
+     ('Slow Waltz', 84, 90, 'A graceful ballroom dance in 3/4 time with smooth, flowing rise-and-fall movements and elegant progress around the floor.'),
+     ('Viennese Waltz', 174, 180, 'A fast, rotating form of waltz in 3/4 time with continuous turns and sweeping movement around the floor; one of the oldest ballroom dances.'),
+     ('Quickstep', 200, 208, 'A brisk, flowing ballroom dance evolved from foxtrot, featuring running steps, syncopations, and light hops; danced in a smooth style with continuous forward movement.'),
+     ('Tango', 200, 208, 'A dramatic ballroom dance with sharp, staccato movements and purposeful pauses, expressing strong character and musical accents.'),
+     ('Cha Cha Cha', 120, 128, 'A lively Latin partner dance with syncopated “cha-cha-cha” steps, playful hip motion, and rhythm that breaks on the 4th and 1st beats.'),
+     ('Rumba', 100, 108, 'A slow, sensual Latin ballroom dance emphasizing hip action and romantic connection, with smooth, deliberate steps and steady rhythm.'),
+     ('Samba', 96, 104, 'A rhythmic Latin dance from Brazil with a distinctive bounce action and forward-backward steps, often seen in both social and competitive dance scenes.'),
+     ('Jive', 168, 184, 'A fast, energetic swing-influenced dance with quick rock-step and triple-step (shuffle) footwork, often danced to upbeat music in social and competitive settings.'),
+     ('Salsa', 180, 300, 'A popular Latin partner dance with energetic footwork, rhythmically syncopated timing, spins, and dynamic lead-follow patterns; danced in clubs worldwide.'),
+     ('Discofox', 112, 120, 'A versatile social partner dance from the disco era, combining simple forward-and-back or side steps with turns and wraps; danced to 4/4 music and easy to pick up by beginners.');
 
  -- Insert songs
 -- INSERT INTO Song (title, speed, dance_id)
