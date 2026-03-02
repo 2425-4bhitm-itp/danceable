f0d640ccabd8351010ba03e96c7ae43d98ad1136
LZinhobel <l.zinhobel@students.htl-leonding.ac.at>
2026-03-02 15:23:11 +0100

    FIX[ml]: fix converter

--- code/ml/utilities/file_converter.py
+++ code/ml/utilities/file_converter.py
@@ -1,13 +1,13 @@
-import moviepy
+from moviepy import AudioFileClip
 
 def convert_webm_to_wav(input_file, output_file):
-    clip = moviepy.VideoFileClip(input_file)
-    clip.audio.write_audiofile(output_file)
+    clip = AudioFileClip(input_file)
+    clip.write_audiofile(output_file)
 
 def convert_caf_to_wav(input_file, output_file):
-    clip = moviepy.AudioFileClip(input_file)
+    clip = AudioFileClip(input_file)
     clip.write_audiofile(output_file)
 
 def convert_mp3_to_wav(input_file, output_file):
-    clip = moviepy.AudioFileClip(input_file)
+    clip = AudioFileClip(input_file)
     clip.write_audiofile(output_file)
